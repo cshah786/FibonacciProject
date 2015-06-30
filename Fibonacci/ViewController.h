45773e8234d1ad8a5e36a63a1e2bbbce08e941ef
Chirag <cshah786@gmail.com>
2015-06-30 18:51:30 -0400

    testing files

--- Fibonacci/ViewController.h
+++ Fibonacci/ViewController.h
@@ -9,7 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *textFieldText;
 
+
+- (IBAction)Login:(UIButton *)sender;
 
 @end
 
--- Fibonacci/ViewController.m
+++ Fibonacci/ViewController.m
@@ -1,4 +1,4 @@
-//
+
 //  ViewController.m
 //  Fibonacci
 //
@@ -24,4 +24,58 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+
+
+- (IBAction)Login:(UIButton *)sender
+{
+    
+    
+        int counter = [self.textFieldText.text intValue];
+//    NSMutableArray* arr = [[NSMutableArray alloc]init];
+//    for (int k =0; k< counter; k++)
+//    {
+//        if(k<2)
+//        {
+//            [arr addObject:[NSString stringWithFormat:@"%d",k]];
+//            continue;
+//        }
+//        else
+//        {
+//            int fib = [[arr objectAtIndex:k-2] intValue] + [[arr objectAtIndex:k-1] intValue];
+//            [arr addObject:[NSString stringWithFormat:@"%d",fib]];
+//        }
+//        
+//    }
+//    
+//    NSLog(@"%@",arr);
+    NSMutableArray *arr = [[NSMutableArray alloc]init];
+    int f1 = 0;
+    int f2 = 1;
+    int fn;
+    
+    for(int i = 0; i<counter;i++)
+    {
+        if(i<2)
+        {
+           
+            [arr addObject:[NSString stringWithFormat:@"%d",i]] ;
+          
+        }
+        
+        else{
+       
+        fn = f1+f2;
+        f1 = f2;
+        f2 = fn;
+        
+        
+        [arr addObject:[NSString stringWithFormat:@"%d",fn]] ;
+        }
+        }
+    NSLog(@"%@",arr);
+
+    
+}
 @end
